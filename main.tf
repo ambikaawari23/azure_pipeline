2bfe62c27173076b5bcb921442052b7ba67b509c
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-09 18:49:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,22 +22,22 @@ module "storage_account" {
   #resource_group_name = azurerm_resource_group.rg.name
 }
 
-# module "key_vault" {
-#   source = "./modules/key_vault"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+module "key_vault" {
+  source = "git::https://github.com/ambikaawari23/key_vault.git"
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# module "app_insight" {
-#   source = "./modules/app_insight"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+module "app_insight" {
+  source = "git::https://github.com/ambikaawari23/app_insight.git"
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# module "app_service" {
-#   source = "./modules/app_service"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+module "app_service" {
+  source = "git::https://github.com/ambikaawari23/app_service.git"
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# module "web_app" {
-#   source = "./modules/web_app"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
+module "web_app" {
+  source = "git::https://github.com/ambikaawari23/web_app.git"
+  resource_group_name = azurerm_resource_group.rg.name
+}
