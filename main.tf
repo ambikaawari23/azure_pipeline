a3e4bd820435d6078a91cbe493d0a4b7b618d155
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-09 15:43:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "storage_account" {
- path ="./storage_account"
+  source ="./storage_account"
   url = "git@github.com/ambikaawari23/storage_account.git"
   resource_group_name = azurerm_resource_group.rg.name
 }
