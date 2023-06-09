1d0a2073c1770611d542a0fabb9f2c2e2429d7b6
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-09 15:22:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.18.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aa-rg-23-${var.location}"
+  location = "West Europe"
+}
+
+module "storage_account" {
+ path ="./storage_account"
+  url = "git@github.com/ambikaawari23/storage_account.git"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# module "key_vault" {
+#   source = "./modules/key_vault"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+# module "app_insight" {
+#   source = "./modules/app_insight"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+# module "app_service" {
+#   source = "./modules/app_service"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+# module "web_app" {
+#   source = "./modules/web_app"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
