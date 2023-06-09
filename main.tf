3a4fe40c194709f911eb3722389df0d011c1ffeb
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-09 20:28:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ resource "azurerm_resource_group" "rg" {
 module "storage_account" { 
   source = "github.com/ambikaawari23/storage_account//storage_account"
   resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
 }
 
 # module "key_vault" {
