7ed593e58afd10aa32ca59dd8d129abe3340acee
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-06-09 16:05:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "aa-rg-23-${var.location}"
+  name     = "aa_rg_23_${var.location}"
   location = "West Europe"
 }
 
